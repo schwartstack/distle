16b2622906938ddaf425a7f34ae018de814200b0
Jonathan Schwartz <schwartstack@MacBook-Pro.local>
2026-08-08 11:43:59 -0700

    make keyboard size a user setting

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "136031787" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2807215216" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
