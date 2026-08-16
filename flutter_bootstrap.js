cfc71dfffd9283333ab9a3999c5526eb35f2bc55
Jonathan Schwartz <schwartstack@MacBook-Pro.local>
2026-08-16 07:24:01 -0700

    adjust colors

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1907377890" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1503621365" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
--- main.dart.js
+++ main.dart.js
@@ -24985,12 +24985,12 @@ p=r.a-s.a
 if(q===0&&p===0)return null
 return Math.atan2(q,p)},
 ayo(a){var s,r,q,p
-A.ayG(B.GB)
-if(a<0.25){s=a/0.25
+A.ayG(B.Gs)
+if(a<0.4){s=a/0.4
 r=B.BL
-q=B.mn}else if(a<0.75){s=(a-0.25)/0.5
+q=B.mn}else if(a<0.8){s=(a-0.4)/0.4
 r=B.mn
-q=B.mr}else{s=(a-0.75)/0.25
+q=B.mr}else{s=(a-0.8)/0.19999999999999996
 r=B.mr
 q=B.BU}p=A.q(r,q,s)
 p.toString
@@ -41249,9 +41249,9 @@ a51(a,b){var s,r=A.ant(a,b)
 r.toString
 s=r/$.asa()
 if(s===0)return"\ud83d\udfe2"
-else if(s<0.25)return"\ud83d\udfe9"
-else if(s<0.5)return"\ud83d\udfe8"
-else if(s<0.75)return"\ud83d\udfe7"
+else if(s<0.26666666666666666)return"\ud83d\udfe9"
+else if(s<0.5333333333333333)return"\ud83d\udfe8"
+else if(s<0.8)return"\ud83d\udfe7"
 return"\ud83d\udfe5"},
 Er(a,b){return this.ag8(a,b)},
 ag8(a,b){var s=0,r=A.N(t.N),q,p=this,o,n,m,l
@@ -41448,7 +41448,7 @@ ak(){var s=null
 return new A.PR(A.rb(!0,s,!0,!0,s,s,!1),A.d1(s,s,B.dd,s,B.hN,s,s,s,s,s,s,s,s,s,s,s,s,!0,s,s,s,s,s,s,s,s))}}
 A.PR.prototype={
 K(a){var s=this,r=null,q=t.p,p=A.dX(A.c([B.E3,A.iK(r,B.db,r,r,new A.ahD(a),r,r,r)],q),B.u,B.Z,0),o=s.e,n=A.apm(o,"If you enjoy this game and you want to support it, you can [buy me a coffee](https://ko-fi.com/schwartstack).",A.d1(r,r,s.a.c?B.k:B.l,r,r,r,r,r,r,r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r)),m=A.cq(r,20,r),l=A.apm(o,"To report a bug or for any other reason: [email me](mailto:jzs1986@gmail.com).",A.d1(r,r,s.a.c?B.k:B.l,r,r,r,r,r,r,r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r)),k=A.cq(r,20,r),j=A.d0("Distle is a Flutter web app created by Jonathan Schwartz, a data scientist from San Jose, California.",r,A.d1(r,r,s.a.c?B.k:B.l,r,r,r,r,r,r,r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r)),i=A.cq(r,20,r)
-return A.he(!0,r,A.vy(r,A.ed(A.c([n,m,l,k,j,i,A.apm(o,"Check out my [website](http://schwartstack.github.io), my [LinkedIn](http://www.linkedin.com/in/schwartstack), and my [GitHub](https://github.com/schwartstack/).",A.d1(r,r,s.a.c?B.k:B.l,r,r,r,r,r,r,r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r)),A.cq(r,30,r),A.jz(new A.zY("version 3.1.0",A.d1(r,r,B.JC,r,r,r,r,r,r,r,r,10,r,r,r,r,r,!0,r,r,r,r,r,r,r,r),r),r,r)],q),B.aJ,B.en,B.ae),!0,p),r,r,r,s.d,!0,r,r,r,new A.ahE(s),r,r)}}
+return A.he(!0,r,A.vy(r,A.ed(A.c([n,m,l,k,j,i,A.apm(o,"Check out my [website](http://schwartstack.github.io), my [LinkedIn](http://www.linkedin.com/in/schwartstack), and my [GitHub](https://github.com/schwartstack/).",A.d1(r,r,s.a.c?B.k:B.l,r,r,r,r,r,r,r,r,r,r,r,r,r,r,!0,r,r,r,r,r,r,r,r)),A.cq(r,30,r),A.jz(new A.zY("version 3.1.1",A.d1(r,r,B.JC,r,r,r,r,r,r,r,r,10,r,r,r,r,r,!0,r,r,r,r,r,r,r,r),r),r,r)],q),B.aJ,B.en,B.ae),!0,p),r,r,r,s.d,!0,r,r,r,new A.ahE(s),r,r)}}
 A.ahE.prototype={
 $2(a,b){var s,r
 if(!(b instanceof A.fj))return B.aK
@@ -85409,20 +85409,21 @@ B.eJ=new A.kn(3,"justify")
 B.ag=new A.kn(4,"start")
 B.hM=new A.kn(5,"end")
 B.Gi=s([B.cb,B.dx,B.hL,B.eJ,B.ag,B.hM],A.am("z<kn>"))
-B.Gs=s([2,1.13276676],t.n)
+B.Gt=s([2,1.13276676],t.n)
 B.Fz=s([2.18349805,1.20311921],t.n)
 B.Hk=s([2.33888662,1.28698796],t.n)
 B.Hm=s([2.48660575,1.36351941],t.n)
 B.Gl=s([2.62226596,1.44717976],t.n)
 B.Gq=s([2.7514899,1.53385819],t.n)
 B.GR=s([3.36298265,1.98288283],t.n)
-B.Gu=s([4.08649929,2.23811846],t.n)
+B.Gv=s([4.08649929,2.23811846],t.n)
 B.GJ=s([4.85481134,2.47563463],t.n)
 B.Go=s([5.62945551,2.72948597],t.n)
-B.Gt=s([6.43023796,2.98020421],t.n)
-B.nr=s([B.Gs,B.Fz,B.Hk,B.Hm,B.Gl,B.Gq,B.GR,B.Gu,B.GJ,B.Go,B.Gt],t.zg)
+B.Gu=s([6.43023796,2.98020421],t.n)
+B.nr=s([B.Gt,B.Fz,B.Hk,B.Hm,B.Gl,B.Gq,B.GR,B.Gv,B.GJ,B.Go,B.Gu],t.zg)
 B.Gk=s([B.ir,B.is],A.am("z<vM>"))
 B.Gn=s([18,15,10,12,15,18,15,12,12],t.n)
+B.Gs=s([0.4,0.8],t.n)
 B.HI=new A.oo("en",null,"US")
 B.ns=s([B.HI],t.ss)
 B.nt=s([0,41,61,101,131,181,251,301,360],t.n)
@@ -85434,7 +85435,6 @@ B.Vj=new A.kC(8,0.12)
 B.Vi=new A.kC(12,0.14)
 B.nu=s([B.Vh,B.Vm,B.Vk,B.Vl,B.Vj,B.Vi],A.am("z<kC>"))
 B.nv=s([0,21,51,121,151,191,271,321,360],t.n)
-B.GB=s([0.25,0.75],t.n)
 B.GC=s([-1,0,0,1,0,0,-1,0,1,0,0,0,-1,1,0,1,1,1,1,0],t.n)
 B.zI=new A.Gk(2,"outer")
 B.m6=new A.m(0.09803921568627451,0,0,0,B.e)
@@ -86150,22 +86150,22 @@ B.Hj=s([B.tl,null,null,B.cC],t.L)
 B.Hl=s([B.tm,null,null,B.em],t.L)
 B.Gc=s([B.eh,B.eh,B.fZ,null],t.L)
 B.Hw=s([B.fV,null,B.fV,null],t.L)
-B.Gv=s([B.bt,null,null,B.cz],t.L)
-B.Gw=s([B.bc,null,null,B.cA],t.L)
-B.Gx=s([B.bd,null,null,B.cB],t.L)
+B.Gw=s([B.bt,null,null,B.cz],t.L)
+B.Gx=s([B.bc,null,null,B.cA],t.L)
+B.Gy=s([B.bd,null,null,B.cB],t.L)
 B.HA=s([B.bu,null,null,B.cC],t.L)
 B.Ho=s([B.jZ,null,null,B.k4],t.L)
 B.Gd=s([B.eg,B.eg,B.fY,null],t.L)
 B.GZ=s([B.ar,null,null,B.cy],t.L)
-B.Gy=s([B.cv,null,null,B.ej],t.L)
+B.Gz=s([B.cv,null,null,B.ej],t.L)
 B.G7=s([B.bM,null,null,B.c5],t.L)
-B.Gz=s([B.cw,null,null,B.el],t.L)
+B.GA=s([B.cw,null,null,B.el],t.L)
 B.Hp=s([B.ef,null,null,B.k3],t.L)
 B.Ge=s([B.ei,B.ei,B.h_,null],t.L)
-B.GA=s([B.ed,null,null,B.ek],t.L)
+B.GB=s([B.ed,null,null,B.ek],t.L)
 B.H4=s([B.ee,null,null,B.em],t.L)
 B.Gf=s([B.c4,B.c4,B.cx,null],t.L)
-B.Jl=new A.cm(["*",B.G4,"+",B.Hi,"-",B.Gr,".",B.GP,"/",B.Ft,"0",B.Hv,"1",B.Hr,"2",B.Ga,"3",B.HB,"4",B.Hq,"5",B.G8,"6",B.Fx,"7",B.Gj,"8",B.Hj,"9",B.Hl,"Alt",B.Gc,"AltGraph",B.Hw,"ArrowDown",B.Gv,"ArrowLeft",B.Gw,"ArrowRight",B.Gx,"ArrowUp",B.HA,"Clear",B.Ho,"Control",B.Gd,"Delete",B.GZ,"End",B.Gy,"Enter",B.G7,"Home",B.Gz,"Insert",B.Hp,"Meta",B.Ge,"PageDown",B.GA,"PageUp",B.H4,"Shift",B.Gf],A.am("cm<E,V<e?>>"))
+B.Jl=new A.cm(["*",B.G4,"+",B.Hi,"-",B.Gr,".",B.GP,"/",B.Ft,"0",B.Hv,"1",B.Hr,"2",B.Ga,"3",B.HB,"4",B.Hq,"5",B.G8,"6",B.Fx,"7",B.Gj,"8",B.Hj,"9",B.Hl,"Alt",B.Gc,"AltGraph",B.Hw,"ArrowDown",B.Gw,"ArrowLeft",B.Gx,"ArrowRight",B.Gy,"ArrowUp",B.HA,"Clear",B.Ho,"Control",B.Gd,"Delete",B.GZ,"End",B.Gz,"Enter",B.G7,"Home",B.GA,"Insert",B.Hp,"Meta",B.Ge,"PageDown",B.GB,"PageUp",B.H4,"Shift",B.Gf],A.am("cm<E,V<e?>>"))
 B.K4={KeyA:0,KeyB:1,KeyC:2,KeyD:3,KeyE:4,KeyF:5,KeyG:6,KeyH:7,KeyI:8,KeyJ:9,KeyK:10,KeyL:11,KeyM:12,KeyN:13,KeyO:14,KeyP:15,KeyQ:16,KeyR:17,KeyS:18,KeyT:19,KeyU:20,KeyV:21,KeyW:22,KeyX:23,KeyY:24,KeyZ:25,Digit1:26,Digit2:27,Digit3:28,Digit4:29,Digit5:30,Digit6:31,Digit7:32,Digit8:33,Digit9:34,Digit0:35,Minus:36,Equal:37,BracketLeft:38,BracketRight:39,Backslash:40,Semicolon:41,Quote:42,Backquote:43,Comma:44,Period:45,Slash:46}
 B.tC=new A.bC(B.K4,["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9","0","-","=","[","]","\\",";","'","`",",",".","/"],t.li)
 B.K0={Abort:0,Again:1,AltLeft:2,AltRight:3,ArrowDown:4,ArrowLeft:5,ArrowRight:6,ArrowUp:7,AudioVolumeDown:8,AudioVolumeMute:9,AudioVolumeUp:10,Backquote:11,Backslash:12,Backspace:13,BracketLeft:14,BracketRight:15,BrightnessDown:16,BrightnessUp:17,BrowserBack:18,BrowserFavorites:19,BrowserForward:20,BrowserHome:21,BrowserRefresh:22,BrowserSearch:23,BrowserStop:24,CapsLock:25,Comma:26,ContextMenu:27,ControlLeft:28,ControlRight:29,Convert:30,Copy:31,Cut:32,Delete:33,Digit0:34,Digit1:35,Digit2:36,Digit3:37,Digit4:38,Digit5:39,Digit6:40,Digit7:41,Digit8:42,Digit9:43,DisplayToggleIntExt:44,Eject:45,End:46,Enter:47,Equal:48,Escape:49,Esc:50,F1:51,F10:52,F11:53,F12:54,F13:55,F14:56,F15:57,F16:58,F17:59,F18:60,F19:61,F2:62,F20:63,F21:64,F22:65,F23:66,F24:67,F3:68,F4:69,F5:70,F6:71,F7:72,F8:73,F9:74,Find:75,Fn:76,FnLock:77,GameButton1:78,GameButton10:79,GameButton11:80,GameButton12:81,GameButton13:82,GameButton14:83,GameButton15:84,GameButton16:85,GameButton2:86,GameButton3:87,GameButton4:88,GameButton5:89,GameButton6:90,GameButton7:91,GameButton8:92,GameButton9:93,GameButtonA:94,GameButtonB:95,GameButtonC:96,GameButtonLeft1:97,GameButtonLeft2:98,GameButtonMode:99,GameButtonRight1:100,GameButtonRight2:101,GameButtonSelect:102,GameButtonStart:103,GameButtonThumbLeft:104,GameButtonThumbRight:105,GameButtonX:106,GameButtonY:107,GameButtonZ:108,Help:109,Home:110,Hyper:111,Insert:112,IntlBackslash:113,IntlRo:114,IntlYen:115,KanaMode:116,KeyA:117,KeyB:118,KeyC:119,KeyD:120,KeyE:121,KeyF:122,KeyG:123,KeyH:124,KeyI:125,KeyJ:126,KeyK:127,KeyL:128,KeyM:129,KeyN:130,KeyO:131,KeyP:132,KeyQ:133,KeyR:134,KeyS:135,KeyT:136,KeyU:137,KeyV:138,KeyW:139,KeyX:140,KeyY:141,KeyZ:142,KeyboardLayoutSelect:143,Lang1:144,Lang2:145,Lang3:146,Lang4:147,Lang5:148,LaunchApp1:149,LaunchApp2:150,LaunchAssistant:151,LaunchControlPanel:152,LaunchMail:153,LaunchScreenSaver:154,MailForward:155,MailReply:156,MailSend:157,MediaFastForward:158,MediaPause:159,MediaPlay:160,MediaPlayPause:161,MediaRecord:162,MediaRewind:163,MediaSelect:164,MediaStop:165,MediaTrackNext:166,MediaTrackPrevious:167,MetaLeft:168,MetaRight:169,MicrophoneMuteToggle:170,Minus:171,NonConvert:172,NumLock:173,Numpad0:174,Numpad1:175,Numpad2:176,Numpad3:177,Numpad4:178,Numpad5:179,Numpad6:180,Numpad7:181,Numpad8:182,Numpad9:183,NumpadAdd:184,NumpadBackspace:185,NumpadClear:186,NumpadClearEntry:187,NumpadComma:188,NumpadDecimal:189,NumpadDivide:190,NumpadEnter:191,NumpadEqual:192,NumpadMemoryAdd:193,NumpadMemoryClear:194,NumpadMemoryRecall:195,NumpadMemoryStore:196,NumpadMemorySubtract:197,NumpadMultiply:198,NumpadParenLeft:199,NumpadParenRight:200,NumpadSubtract:201,Open:202,PageDown:203,PageUp:204,Paste:205,Pause:206,Period:207,Power:208,PrintScreen:209,PrivacyScreenToggle:210,Props:211,Quote:212,Resume:213,ScrollLock:214,Select:215,SelectTask:216,Semicolon:217,ShiftLeft:218,ShiftRight:219,ShowAllWindows:220,Slash:221,Sleep:222,Space:223,Super:224,Suspend:225,Tab:226,Turbo:227,Undo:228,WakeUp:229,ZoomToggle:230}
